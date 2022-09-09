5f1e2c624d40c2235d571a34b986b10bd35b8e08
ThiagoPSs <psthigo@gmail.com>
2022-09-08 21:27:51 -0300

    final

--- app/src/main/java/com/example/atividadequiz/erro.java
+++ app/src/main/java/com/example/atividadequiz/erro.java
@@ -3,18 +3,81 @@ package com.example.atividadequiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.Random;
+
 public class erro extends AppCompatActivity {
+
+    SensorManager sensorManager;
+    Sensor sensor;
+    SensorEventListener sensorEventListener;
+    int whip=0;
+    Intent intent = new Intent(this, MainActivity.class);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_erro);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        if (sensor == null) ;
+
+        sensorEventListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                float x = sensorEvent.values[0];
+
+                if (x < -5 && whip == 0) {
+                    whip++;
+
+
+
+                } else if (x > 5 && whip == 1) {
+                    whip++;
+                    startActivity(intent);
+                }
+                if(whip==2){
+                    startActivity(intent);
+                    whip=0;
+                }
+
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+
+            }
+        };
+    }
+    private void start(){
+        sensorManager.registerListener(sensorEventListener,sensor,SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    private void stop(){
+        sensorManager.unregisterListener(sensorEventListener);
     }
     public void voltar(View d){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
+
+    @Override
+    protected void onPause(){
+        stop();
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume(){
+        start();
+        super.onResume();
+
+    }
 }
+
